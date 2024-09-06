0c1b82bc428792a2f230adca51c7e2ab45475522
DonDonDon02 <kahowow73@gmail.com>
2024-09-06 12:39:05 +0800

    1

---
+++ main.py
@@ -0,0 +1,37 @@
+import pyodbc
+import os 
+from dotenv import load_dotenv
+import pandas as pd
+import streamlit as st
+
+
+load_dotenv()
+
+server = os.getenv('DATABASE_SER')
+database = 'mySampleDatabase'
+username = os.getenv('DATABASE_USERNAME')
+password = os.getenv('DATABASE_PASSWORD')
+driver = '{ODBC Driver 18 for SQL Server}'
+ 
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+    with conn.cursor() as cursor:
+        # Execute the query
+        cursor.execute('''
+            SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+            FROM SalesLT.ProductCategory pc
+            JOIN SalesLT.Product p
+            ON pc.productcategoryid = p.productcategoryid;
+        ''')
+
+        # Fetch all rows
+        rows = cursor.fetchall()
+
+        # Convert to DataFrame
+        df = pd.DataFrame.from_records(rows, columns=[desc[0] for desc in cursor.description])
+
+# Display the DataFrame
+st.write(df)
+
+
+
+
