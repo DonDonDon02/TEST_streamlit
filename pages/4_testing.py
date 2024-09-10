a1db81b8668668d3d7112f96e5918d47f8d6ab19
Donavan02 <kahowow73@gmail.com>
2024-09-10 16:36:25 +0800

    1

--- pages/4_testing.py
+++ pages/4_testing.py
@@ -6,6 +6,16 @@ import yfinance as yf
 import datetime
 import plotly.express as px 
 
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    st.metric("Close Price", 1)
+with col2:
+    st.metric("Price Difference (YoY)", 2)
+with col3:
+    st.metric("52-Week High",2)
+with col4:
+    st.metric("52-Week Low", 2)
+
 
 def get_stock_info(ticker):
     stock = yf.Ticker(ticker)
@@ -17,3 +27,4 @@ def get_stock_info(ticker):
     return stock.info
 
 st.write(get_stock_info('aapl'))
+
