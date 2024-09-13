38fd67a9eaeccdd1c3506284842b4a63b3353e89
DonDonDon02 <kahowow73@gmail.com>
2024-09-13 17:09:40 +0800

    Update 5_testing2.py

--- pages/5_testing2.py
+++ pages/5_testing2.py
@@ -1,21 +1,101 @@
-import streamlit as st
+import streamlit as st 
 import pandas as pd
+import datetime
+import yfinance as yf 
+
+@st.cache_data
+def getsp500_Symbol():
+    sp500_csv_= pd.read_csv('https://raw.githubusercontent.com/datasets/s-and-p-500-companies/main/data/constituents.csv')
+    return sp500_csv_
+
+
+@st.cache_data
+def nq_Symbol():
+    nasdaq_csv_= pd.read_csv('https://raw.githubusercontent.com/datasets/nasdaq-listings/master/data/nasdaq-listed.csv')
+    return nasdaq_csv_
+
+
+chosen = st.sidebar.radio('Select', ("S&P 500", "Nasdaq"))
+
+if chosen == 'S&P 500':
+    options = st.sidebar.selectbox(
+        "Select S&P 500 Symbol",
+        getsp500_Symbol()['Symbol'].unique().tolist(),
+    )
+else:  # chosen == 'Nasdaq'
+    options = st.sidebar.selectbox(
+        "Select Nasdaq Symbol",
+        nq_Symbol()['Symbol'].unique().tolist(),
+        
+    )
+
+
+
+buy_date = st.sidebar.date_input("buy_date", datetime.date(2019, 1, 2))
+buy_date_next = buy_date + datetime.timedelta(days=1)
+
+sell_date = st.sidebar.date_input("sell_date", datetime.date.today())
+sell_date_next = sell_date + datetime.timedelta(days=1)
+
+    
+@st.cache_data
+def get_price(code,date1,date2):
+    stock = yf.Ticker(code)
+    data = stock.history(period='1d', start=date1, end=date2)
+    if data.empty:
+            st.warning(f"No data found for {code}. This symbol may be delisted.")
+            return pd.DataFrame()  # Return an empty DataFrame
+    data.reset_index(inplace=True)
+    data['Date'] = data['Date'].dt.strftime('%Y/%m/%d')
+    data["Code"] = code
+    data = data.drop(columns=['Dividends', 'Stock Splits'])
+    data = data.round(2)
+    return data
+
+
+
+
+##################################### 
+#####################################
 
 # Initialize session state to store a DataFrame
 if 'df' not in st.session_state:
-    st.session_state.df = pd.DataFrame(columns=['Name', 'Age'])
+    st.session_state.df = pd.DataFrame(columns=['Code','Date', 'Close','Number of Shares','Total Value'])
 
 # Function to add new row to DataFrame
-def add_data(name, age):
-    new_row = {'Name': name, 'Age': age}
+def add_data(Code ,Date ,Close ,Number_of_Shares ,Total_Value ,):
+    new_row = {'Code':Code ,'Date':Date, 'Close':Close,'Number of Shares':Number_of_Shares,'Total Value':Total_Value}
     st.session_state.df = st.session_state.df._append(new_row, ignore_index=True)
 
 # User input for Name and Age
 name = st.text_input("Enter name")
-age = st.number_input("Enter age", min_value=0, step=1)
 
+try:
+    data = get_price(options,buy_date,buy_date_next)
+except:
+    st.warning("weekend market close / Not avilble ")
+Number_of_Shares_ = st.number_input("Number_of_Shares_", min_value=1, step=1)
+
+Code = data['Code'].iloc[0]
+Date = data['Date'].iloc[0]
+Close = data['Close'].iloc[0]
+Number_of_Shares = Number_of_Shares_
+Total_Value = Number_of_Shares_ * Close
+
+st.write(get_price(options,buy_date,buy_date_next))
 if st.button("Add Data"):
-    add_data(name, age)
+    add_data(Code ,Date ,Close ,Number_of_Shares ,Total_Value ,)
 
 # Display the DataFrame
 st.write(st.session_state.df)
+
+
+grouped = st.session_state.df.groupby('Code').agg({
+    'Number of Shares': 'sum',
+    'Total Value': 'sum',
+    'Close': 'mean'
+}).reset_index()
+
+grouped = grouped.rename(columns={'Close': 'Avg Price'})
+
+st.write(grouped)
