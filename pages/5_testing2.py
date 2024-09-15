3c0be1021456030fe373cd480e64d5695f6e065e
DonDonDon02 <kahowow73@gmail.com>
2024-09-16 00:35:53 +0800

    1

--- pages/5_testing2.py
+++ pages/5_testing2.py
@@ -5,136 +5,142 @@ import yfinance as yf
 
 st.set_page_config(layout="wide")
 
-
+@st.cache_data
+def get_sp500_symbols():
+    sp500_csv = pd.read_csv('https://raw.githubusercontent.com/datasets/s-and-p-500-companies/main/data/constituents.csv')
+    return sp500_csv
 
 @st.cache_data
-def getsp500_Symbol():
-    sp500_csv_= pd.read_csv('https://raw.githubusercontent.com/datasets/s-and-p-500-companies/main/data/constituents.csv')
-    return sp500_csv_
-
-
-@st.cache_data
-def nq_Symbol():
-    nasdaq_csv_= pd.read_csv('https://raw.githubusercontent.com/datasets/nasdaq-listings/master/data/nasdaq-listed.csv')
-    return nasdaq_csv_
-
+def get_nasdaq_symbols():
+    nasdaq_csv = pd.read_csv('https://raw.githubusercontent.com/datasets/nasdaq-listings/master/data/nasdaq-listed.csv')
+    return nasdaq_csv
 
 chosen = st.sidebar.radio('Select', ("S&P 500", "Nasdaq"))
 
 if chosen == 'S&P 500':
     options = st.sidebar.selectbox(
         "Select S&P 500 Symbol",
-        getsp500_Symbol()['Symbol'].unique().tolist(),
+        get_sp500_symbols()['Symbol'].unique().tolist(),
     )
 else:  # chosen == 'Nasdaq'
     options = st.sidebar.selectbox(
         "Select Nasdaq Symbol",
-        nq_Symbol()['Symbol'].unique().tolist(),
-        
+        get_nasdaq_symbols()['Symbol'].unique().tolist(),
     )
 
-
-
-buy_date = st.sidebar.date_input("buy_date", datetime.date(2019, 1, 2))
+buy_date = st.sidebar.date_input("Buy Date", datetime.date(2019, 1, 2))
 buy_date_next = buy_date + datetime.timedelta(days=1)
 
-sell_date = st.sidebar.date_input("sell_date", datetime.date.today())
+sell_date = st.sidebar.date_input("Sell Date", datetime.date.today())
 sell_date_next = sell_date + datetime.timedelta(days=1)
 
-    
 @st.cache_data
-def get_price(code,date1,date2):
-    stock = yf.Ticker(code)
-    data = stock.history(period='1d', start=date1, end=date2)
+def fetch_stock_price(symbol, start_date, end_date):
+    stock = yf.Ticker(symbol)
+    data = stock.history(period='1d', start=start_date, end=end_date)
     if data.empty:
-            st.warning(f"No data found for {code}. This symbol may be delisted.")
-            return pd.DataFrame()  # Return an empty DataFrame
+        st.warning(f"No data found for {symbol}. This symbol may be delisted.")
+        return pd.DataFrame()  # Return an empty DataFrame
     data.reset_index(inplace=True)
     data['Date'] = data['Date'].dt.strftime('%Y/%m/%d')
-    data["Code"] = code
+    data["Symbol"] = symbol
     data = data.drop(columns=['Dividends', 'Stock Splits'])
     data = data.round(2)
     return data
 
-
-
-
-##################################### 
-#####################################
-
 # Initialize session state to store a DataFrame
-if 'df' not in st.session_state:
-    st.session_state.df = pd.DataFrame(columns=['Code','Date', 'Close','Number of Shares','Total Value'])
+if 'buy_record' not in st.session_state:
+    st.session_state.buy_record = pd.DataFrame(columns=['Symbol', 'Date', 'Close', 'Number of Shares', 'Total Value'])
+    
+if 'sell_record' not in st.session_state:
+    st.session_state.sell_record = pd.DataFrame(columns=['Symbol', 'Date', 'Close', 'Number of Shares', 'Total Value'])
 
 # Function to add new row to DataFrame
-def add_data(Code ,Date ,Close ,Number_of_Shares ,Total_Value ,):
-    new_row = {'Code':Code ,'Date':Date, 'Close':Close,'Number of Shares':Number_of_Shares,'Total Value':Total_Value}
-    st.session_state.df = st.session_state.df._append(new_row, ignore_index=True)
+def add_buy_record(symbol, date, close, number_of_shares, total_value):
+    new_row = {'Symbol': symbol, 'Date': date, 'Close': close, 'Number of Shares': number_of_shares, 'Total Value': total_value}
+    st.session_state.buy_record = st.session_state.buy_record._append(new_row, ignore_index=True)
 
-# User input for Name and Age
+def add_sell_record(symbol, date, close, number_of_shares, total_value):
+    new_row = {'Symbol': symbol, 'Date': date, 'Close': close, 'Number of Shares': number_of_shares, 'Total Value': total_value}
+    st.session_state.sell_record = st.session_state.sell_record._append(new_row, ignore_index=True)
 
-col1, col2= st.columns(2)
+# User input for buying
+col1, col2 = st.columns(2)
 
 with col1:
+    buy_data = fetch_stock_price(options, buy_date, buy_date_next)
+    number_of_shares_buy = st.number_input("Number of Shares to Buy", min_value=1, step=1)
 
-    try:
-        data = get_price(options,buy_date,buy_date_next)
-    except:
-        st.warning("weekend market close / Not avilble ")
-    Number_of_Shares_ = st.number_input("Number_of_Shares_Buy", min_value=1, step=1)
+    if not buy_data.empty:
+        buy_code = buy_data['Symbol'].iloc[0]
+        buy_date_value = buy_data['Date'].iloc[0]
+        buy_close_value = buy_data['Close'].iloc[0]
+        total_value_buy = number_of_shares_buy * buy_close_value
 
-    Code_buy = data['Code'].iloc[0]
-    Date_buy = data['Date'].iloc[0]
-    Close_buy = data['Close'].iloc[0]
-    Number_of_Shares_buy = Number_of_Shares_
-    Total_Value_buy = Number_of_Shares_ * Close_buy
+        st.write(buy_data[['Date', 'Close', 'Symbol']])
+        if st.button("Buy"):
+            add_buy_record(buy_code, buy_date_value, buy_close_value, number_of_shares_buy, total_value_buy)
 
-    st.write(get_price(options,buy_date,buy_date_next))
-    if st.button("Buy"):
-        add_data(Code_buy ,Date_buy ,Close_buy ,Number_of_Shares_buy ,Total_Value_buy ,)
-
-
-
-    with st.expander("Show buy record"):
-        st.write(st.session_state.df)
-        
+    with st.expander("Show Buy Record"):
+        st.write(st.session_state.buy_record)
 
 with col2:
+    sell_data = fetch_stock_price(options, sell_date, sell_date_next)
+    number_of_shares_sell = st.number_input("Number of Shares to Sell", min_value=1, step=1)
 
-    try:
-        data = get_price(options,buy_date,buy_date_next)
-    except:
-        st.warning("weekend market close / Not avilble ")
-    Number_of_Shares_Sell = st.number_input("Number_of_Shares_Sell", min_value=1, step=1)
+    if not sell_data.empty:
+        sell_code = sell_data['Symbol'].iloc[0]
+        sell_date_value = sell_data['Date'].iloc[0]
+        sell_close_value = sell_data['Close'].iloc[0]
+        total_value_sell = number_of_shares_sell * sell_close_value
 
-    Code_buy = data['Code'].iloc[0]
-    Date_buy = data['Date'].iloc[0]
-    Close_buy = data['Close'].iloc[0]
-    Number_of_Shares_buy = Number_of_Shares_Sell
-    Total_Value_buy = Number_of_Shares_Sell * Close_buy
+        st.write(sell_data[['Date', 'Close', 'Symbol']])
+        if st.button("Sell"):
+            add_sell_record(sell_code, sell_date_value, sell_close_value, number_of_shares_sell, total_value_sell)
+                
 
-    st.write(get_price(options,buy_date,buy_date_next))
-    if st.button("Sell"):
-        add_data(Code_buy ,Date_buy ,Close_buy ,Number_of_Shares_buy ,Total_Value_buy ,)
-
-
-
-    with st.expander("Show sell record"):
-        st.write(st.session_state.df)
+    with st.expander("Show Sell Record"):
+        st.write(st.session_state.sell_record)
 
 
+col1 ,col2 = st.columns(2)
+with col1 :
 
-grouped = st.session_state.df.groupby('Code').agg({
-    'Number of Shares': 'sum',
-    'Total Value': 'sum',
+    grouped_buy = st.session_state.buy_record.groupby('Symbol').agg({
+        'Number of Shares': 'sum',
+        'Total Value': 'sum',
+    }).reset_index()
 
-}).reset_index()
+    grouped_buy['Avg Price'] = grouped_buy['Total Value'] / grouped_buy['Number of Shares']
 
-grouped['avg price'] = grouped['Total Value'] / grouped['Number of Shares']
+    st.title('holding')
+    st.write(grouped_buy)
 
-st.write(' portfolio position')
-st.write(grouped)
+with col2:
+    grouped_sell = st.session_state.sell_record.groupby('Symbol').agg({
+        'Number of Shares': 'sum',
+        'Total Value': 'sum',
+    }).reset_index()
 
+    grouped_sell['Avg Price'] = grouped_sell['Total Value'] / grouped_sell['Number of Shares']
 
+    st.title('selling')
+    st.write(grouped_sell)
+    
 
+profo = pd.DataFrame({
+    'Symbol': grouped_buy['Symbol'],
+    'Shares Held': grouped_buy['Number of Shares'] - (grouped_sell['Number of Shares'] if not grouped_sell['Number of Shares'].empty else 0),
+    'Holding Value': grouped_buy['Total Value'],
+    'Selling Value': grouped_sell['Total Value'],
+    'Difference in Value': grouped_sell['Total Value'] - grouped_buy['Total Value'],
+    'Difference in Shares': grouped_sell['Number of Shares'] - grouped_buy['Number of Shares']
+})
 
+st.write(profo)
+
+# Reset button
+if st.sidebar.button("Reset All Records"):
+    st.session_state.buy_record = pd.DataFrame(columns=['Symbol', 'Date', 'Close', 'Number of Shares', 'Total Value'])
+    st.session_state.sell_record = pd.DataFrame(columns=['Symbol', 'Date', 'Close', 'Number of Shares', 'Total Value'])
+    st.success("All records have been reset.")
